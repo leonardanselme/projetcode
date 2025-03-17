75e363cbb7c0155115db9d99ad4c245c44e318ed
leonardanselme <leonardanselme12@gmail.com>
2025-03-17 15:37:32 +0100

    push before Vercel

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import ButtonLogin from "@/components/ButtonLogin";
 import FAQListItem from "@/components/FAQListItem";
+import Image from "next/image";
+import productDemo from "@/app/productDemo.jpeg";
 
 export default function Home() {
   const isLoggedIn = true;
@@ -10,31 +12,41 @@ export default function Home() {
   return (
     <main>
       {/*HEADER*/}
-      <section className="bg-base-200 flex justify-between items-center px-8 py-2">
-        <div className="font-bold">Leo Saas</div>
-        <div className="space-x-4 max-md:hidden ">
-          <a className="link link-hover" href="#faq">
-            FAQ
-          </a>
-          <a className="link link-hover" href="#pricing">
-            Pricing
-          </a>
-        </div>
-        <div>
-          <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      <section className="bg-base-200">
+        <div className="max-w-5xl mx-auto flex justify-between items-center px-8 py-2">
+          <div className="font-bold">Leo Saas</div>
+          <div className="space-x-4 max-md:hidden ">
+            <a className="link link-hover" href="#faq">
+              FAQ
+            </a>
+            <a className="link link-hover" href="#pricing">
+              Pricing
+            </a>
+          </div>
+          <div>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
         </div>
       </section>
       {/*HERO*/}
-      <section className="text-center py-32 px-8 max-w-3xl mx-auto">
-        <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
-          Collect customer feedback to build better projects
-        </h1>
-        <div className="opacity-90 mb-10">
-          Create a feedback board in minutes, prioritize features, and build
-          products that your customers will love.
-        </div>
+      <section className="text-center lg:text-left py-32 px-8 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+        <Image
+          src={productDemo}
+          alt="Product demo"
+          className="w-96 rounded-xl"
+        />
 
-        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+        <div>
+          <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
+            Collect customer feedback to build better projects
+          </h1>
+          <div className="opacity-90 mb-10">
+            Create a feedback board in minutes, prioritize features, and build
+            products that your customers will love.
+          </div>
+
+          <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+        </div>
       </section>
       {/*PRICING*/}
 
