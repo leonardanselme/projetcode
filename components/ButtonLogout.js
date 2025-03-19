0a7bc137a025897e2f25712ebfe56c4cb8badeae
leonardanselme <leonardanselme12@gmail.com>
2025-03-19 18:29:55 +0100

    fixed ButtonLogout

--- components/ButtonLogout.js
+++ components/ButtonLogout.js
@@ -5,7 +5,7 @@ import { signOut } from "next-auth/react"; // ATTENTION, bien importer signOut c
 //Le bouton est accessible côté client, donc on veut être sûr que le bouton ne puisse pas être utilisé côté serveur.
 //Et que le client ait accès à des informations sensibles comme les clés d'API.
 
-const ButtonLogout = ({}) => {
+const ButtonLogout = () => {
   return (
     <button className="btn btn-ghost" onClick={() => signOut()}>
       Logout
